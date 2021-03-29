cb6df04cde1ca6a04382a9b1ca3ffd2297b6df3a
Mudassir Ali <mudassirali36@gmail.com>
2021-03-29 17:23:29 +0500

    deployment script updated

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 import { Contract, ContractFactory } from "ethers";
 
 async function main(): Promise<void> {
-  // Hardhat always runs the compile task when running scripts through it.
-  // If this runs in a standalone fashion you may want to call compile manually
-  // to make sure everything is compiled
-  // await run("compile");
+  const [deployer] = await ethers.getSigners();
 
-  const INITIAL_MINTER = "0x9D1040827fD85ccd3B59Ab96FD8CAf0dbFcAC44e"; //Ganache address
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
 
-  // We get the contract to deploy
-  const Token: ContractFactory = await ethers.getContractFactory("ContractName");
-  console.log(`Start deploying ERC20 with initial minter: ${INITIAL_MINTER}`);
-  const token: Contract = await Token.deploy(INITIAL_MINTER);
+  const Token: ContractFactory = await ethers.getContractFactory("Token");
+  const token: Contract = await Token.deploy();
+  
   await token.deployed();
 
-  console.log("Token deployed to: ", token.address);
+  console.log("Contract deployed to: ", token.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error: Error) => {
